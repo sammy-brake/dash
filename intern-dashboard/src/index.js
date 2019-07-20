194be3b653fbfb38ac6df32ecd73e945c9a17810
sammy-brake <sammy.brake@gmail.com>
2019-07-20 18:22:47 -0500

    set up store.js and wrap index in provider

--- intern-dashboard/src/index.js
+++ intern-dashboard/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store.js';
 
 
 
 ReactDOM.render(
-<App />, 
+    
+        <Provider store={store}>
+            <App />
+        </Provider>,
+
 document.getElementById('root')
 );
 
---
+++ intern-dashboard/src/store.js
@@ -0,0 +1,20 @@
+import {
+    createStore,
+    applyMiddleware,
+    combineReducers
+  } from 'redux';
+  import thunk from 'redux-thunk';
+//  import all of your reducers from your reducers files
+  
+  
+  const reducers = combineReducers({
+    // use your reducers here 
+  });
+  
+  const middleware = [thunk];
+  
+  export default createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware)
+  );
