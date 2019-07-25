ffdf936cea12c7db1b703921a62a14e6a42d0973
sammy-brake <sammy.brake@gmail.com>
2019-07-25 00:57:22 -0500

    change intern hours form into a presentational component

--- intern-dashboard/src/components/InternHours.js
+++ intern-dashboard/src/components/InternHours.js
@@ -1,58 +1,17 @@
 
     import React, { Component } from 'react';
-    import { connect } from 'react-redux';
-    import {editIntern} from '../actions/editIntern'
+   
+    const InternHours = (props) => (
     
-    
-    class InternHours extends Component {
-    
-        state = {
-            hours: null,
-            internId: this.props.internId,
-            visibility: "visible"
-        }
-    
-        handleChange = event => {
-            this.setState({
-                hours: event.target.value,
-                internId: this.props.internId
-            });
-            }
-    
-            handleSubmit = event => {
-               event.preventDefault();
-                this.props.editIntern(this.state);
-                this.setState({
-                    hours: null,
-                    internId: this.props.internId,
-                    visibility: "hidden"
-            });  
-        };
-
-
-   render() {
-       debugger
-     return (
     <div >
-        <form className="intern-hours"  onSubmit={ event => this.props.handleSubmit(event) }>
+        <form className="intern-hours" style={{visibility: props.visibility}}  onSubmit={ event => props.handleSubmit(event) }>
         <label>Update Hours: </label>
-            <input type="number" value={this.props.hours} onChange={this.props.handleChange}/>
-            <input type="submit" style={{visibility: this.state.visibility}}/>
+            <input type="number" value={props.hours} onChange={props.handleChange}/>
+            <input type="submit" style={{visibility: props.visibility}}/>
         </form>
         
     </div>
     )
 
-     }
-}
-InternHours.defaultProps = {
-    interns: []
-}
+export default InternHours
 
-const mapStateToProps = state => {
-    return {
-        interns: state.interns.interns,
-    }
-}
-
-export default connect(mapStateToProps, {editIntern})(InternHours);
